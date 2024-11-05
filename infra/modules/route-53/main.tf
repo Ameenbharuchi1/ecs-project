7b53c5c74ce09f3a00f54f8fa5bfc345702bd8dd
Ameen Bharuchi <ameenbharuchi@Ameens-MBP-2.communityfibre.co.uk>
2024-11-05 22:16:04 +0000

    changed build yaml

--- infra/modules/route-53/main.tf
+++ infra/modules/route-53/main.tf
@@ -2,8 +2,6 @@ data "aws_route53_zone" "hz" {
   name = var.hosted_zone  
 }
 
-
-
 resource "aws_route53_record" "tm_subdomain" {
   zone_id = data.aws_route53_zone.hz.zone_id
   name    = var.subdomain_name
